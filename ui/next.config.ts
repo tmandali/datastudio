77f7ea8e11cbc59a0fad1b922de05a303962e312
timur-mandali_lcw <timur.mandali@lcwaikikiki.com>
2026-01-06 04:49:59 +0300

    k

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["10.60.52.36", "timurmandali.lcwaikiki.local"],
+  allowedDevOrigins: ["10.60.52.36", "timurmandali.lcwaikiki.local", "timurmandali"],
 };
 
 export default nextConfig;
